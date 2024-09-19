37f694634d05157f4ecb2e628774a4a0804894ff
Christian <cristlethedjfurry@gmail.com>
2024-09-19 14:40:47 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        int numToExamine = 8;
+        numToExamine = numToExamine % 2;
+        System.out.println("The number to be examined is either 1 for odd, or 0 for even. It is " + numToExamine);
+    }
+}
